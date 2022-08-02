e16b3a78beacd069b26932410ce3922c93bbb79a
nyambura00 <winnienyambura14@gmail.com>
2022-08-02 15:59:09 +0300

    set up /public

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
