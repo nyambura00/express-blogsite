45e6717e2b2a91aa5a6969f5fb9c7c7e18ad098f
nyambura00 <winnienyambura14@gmail.com>
2022-08-17 14:14:07 +0300

    handle users/register validations

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -4,6 +4,8 @@ const path = require('path');
 module.exports = (req,res)=>{
     User.create(req.body, (error, user) => {
         if(error){
+            const validationError = Object.keys(error.errors).map(key => error.errors[key].message) //handling error
+            req.session.validationErrors = validationErrors;
             return res.redirect('/auth/register')
         }
         res.redirect('/');
